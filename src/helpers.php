204319f928bd8014287a57f636edcd0af3340b5c
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-06-28 14:24:54 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,17 @@
 <?php
 
+use Mpietrucha\Events\Context;
 use Mpietrucha\Events\Component\Event;
 use Mpietrucha\Events\Component\Dispatcher;
 
 if (! function_exists('event_aware')) {
-    function event_aware(string $name, Closure $callback): void {
-        Event::$name($callback);
+    function event_aware(string $name, Closure $callback): Context {
+        return Event::$name($callback);
     }
 }
 
 if (! function_exists('dispatch_aware')) {
-    function dispatch_aware(string $name): void {
-        Dispatcher::$name();
+    function dispatch_aware(string $name): Context {
+        return Dispatcher::$name();
     }
 }
