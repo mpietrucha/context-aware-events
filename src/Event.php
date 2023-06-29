2565f171cdc89ab006a74196dc90704b32f612be
Michał Pietrucha <michal.pietrucha@off-site.pl>
2023-06-29 14:41:55 +0200

    wip

--- src/Event.php
+++ src/Event.php
@@ -4,10 +4,13 @@ namespace Mpietrucha\Events;
 
 use Mpietrucha\Support\Artisan;
 use Mpietrucha\Support\Cli;
+use Mpietrucha\Support\Concerns\HasFactory;
 use Mpietrucha\Events\Contracts\EventInterface;
 
 abstract class Event implements EventInterface
 {
+    use HasFactory;
+
     protected ?Cli $cli = null;
 
     public function cli(): Cli
